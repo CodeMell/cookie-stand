fcd99c69375bfb39c8830122a6b97f42fcfbc0c3
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-03-01 04:31:37 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-function generateRandomNum{
-    return Math.floor(Math.random() * max);
-}
+let cp = {
+    //customers per hour
+    cph: {
+        Math.random()
+    },
+    //min hourly customers
+    min:1,
+    //max hourly customers
+    max: 70,
+    //average cookies per customer
+    average: {
+
+    },
+};
+
+
+// function generateRandomNum{
+//     return Math.floor(Math.random() * max);
+// }
